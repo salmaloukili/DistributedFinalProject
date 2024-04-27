d49f180141e6ed21d6bd78add8624ff8237dd00e
nicogusuarez <nicolas.gutierrezsuarez@student.kuleuven.be>
2024-04-27 19:06:21 +0200

    Added templates we can use

---
+++ sites/rental-site/vue.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  chainWebpack: config => {
+    config.plugin("html").tap(args => {
+      args[0].title = "Ecomart - Home";
+      return args;
+    });
+  },
+  configureWebpack: {
+    externals: {
+      jquery: "jQuery"
+    }
+  },
+  pwa: {
+    name: "Ecomart",
+    themeColor: "#5f1fad",
+    assetsVersion: "3",
+    manifestOptions: {},
+    workboxOptions: {
+      skipWaiting: true
+    }
+  }
+};
