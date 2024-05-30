b49211638dad4210880a2fcd0e51e49b2a8ee5d0
Nicolas Gutierrez <nicogusuarez@gmail.com>
2024-05-30 13:17:05 +0200

    Moved files

--- src/api/src/index.ts
+++ src/api/src/index.ts
@@ -1,21 +1,8 @@
-import express, { Application, Request, Response } from "express";
-// import { Model } from "coloquent";
-import { RecordSchema } from "@orbit/records";
-import { JSONAPISource } from "@orbit/jsonapi";
 import { onCall } from "firebase-functions/v1/https";
-import * as testing from "./testing";
+import * as testing from "./functions/testing";
 
-
-
-const app: Application = express();
-const router = express.Router();
-
-app.use("/api", router);
-
-// export const functions_api = functions
-//   .region("europe-west1")
-//   .https.onRequest(app);
 exports.getGreeting = onCall((request) => {
   return "Hello, world!";
 });
+
 exports.test = testing;
--- src/api/src/queries.ts
+++
@@ -1,9 +0,0 @@
-// const users = source.query((q) => q.findRecords("Employee"));
-// users
-//   .then((r) => {
-//     res.json(r);
-//   })
-//   .catch((e) => {
-//     console.log(e);
-//     res.send(`${e}`);
-//   });
