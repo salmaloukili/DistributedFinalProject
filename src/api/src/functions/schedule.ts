226a3f7ad6a744036513801a9270777d3a0e1676
Nicolas Gutierrez <nicogusuarez@gmail.com>
2024-05-31 21:57:58 +0200

    Added greater than

--- src/api/src/functions/schedule.ts
+++ src/api/src/functions/schedule.ts
@@ -1,88 +1,63 @@
 import * as functions from "firebase-functions";
 import sources from "../orbit/sources";
+import { JSONAPISource } from "@orbit/jsonapi";
 
-exports.queryCatering = functions
-  .region("europe-west1")
-  .https.onRequest(async (req, res) => {
-    const data = [];
-    let query = sources.transport[0].query(
-      (q) => q.findRecords("Bus")
-      // .page({ offset: 0, limit: 1 })
-      // .options({ include: ["events"] })
-    );
-    data[0] = await query;
-
-    query = sources.transport[0].query((q) =>
-      q
-        .findRecords("Schedule")
-        // .page({ offset: 0, limit: 1 })
-        .options({ include: ["bus"] })
-    );
-    data[1] = await query;
-
-    query = sources.transport[0].query((q) =>
-      q
-        .findRecords("Seat")
-        // .page({ offset: 0, limit: 1 })
-        .options({ include: ["schedule"] })
-    );
-    data[2] = await query;
-
-    res.json(data);
-  });
+interface JSONAPIParams {
+  obj: string;
+  time?: Date;
+  include?: string[];
+}
+async function getNewData(sources: JSONAPISource[], params: JSONAPIParams[]) {
+  const data: any = [];
+  const date = new Date();
+  date.setHours(10);
+  for (let i = 0; i < sources.length; i++) {
+    const source = sources[i];
+    for (let j = 0; j < params.length; j++) {
+      const query = source.query((q) =>
+        q.findRecords(params[j].obj).filter({
+          attribute: "modified_at",
+          op: "gt",
+          value: date.toJSON().replace("T", " "),
+        })
+      );
+      data.push(await query);
+    }
+    return data;
+  }
+}
 
 exports.queryTransport = functions
   .region("europe-west1")
   .https.onRequest(async (req, res) => {
-    const data = [];
-    let query = sources.catering[0].query(
-      (q) => q.findRecords("Menu").sort({ attribute: "modified_at" })
-      // .page({ offset: 0, limit: 1 })
-      .options({ include: ["meals"] })
+    res.json(
+      await getNewData(sources.transport, [
+        { obj: "Bus" },
+        { obj: "Schedule" },
+        { obj: "Seat" },
+      ])
     );
-    data[0] = await query;
+  });
 
-    query = sources.catering[0].query((q) =>
-      q
-        .findRecords("Meal")
-        // .page({ offset: 0, limit: 1 })
-        .options({ include: ["menu"] })
+exports.queryCatering = functions
+  .region("europe-west1")
+  .https.onRequest(async (req, res) => {
+    res.json(
+      await getNewData(sources.catering, [{ obj: "Menu" }, { obj: "Meal" }])
     );
-    data[1] = await query;
-
-    res.json(data);
   });
 
 exports.queryVenues = functions
   .region("europe-west1")
   .https.onRequest(async (req, res) => {
-    const data = [];
-    let query = sources.venues[0].query(
-      (q) => q.findRecords("Venue")
-      // .page({ offset: 0, limit: 1 })
-      // .options({ include: ["events"] })
+    res.json(
+      await getNewData(sources.transport, [
+        { obj: "Venue" },
+        { obj: "Event" },
+        { obj: "Ticket" },
+      ])
     );
-    data[0] = await query;
-
-    query = sources.venues[0].query((q) =>
-      q
-        .findRecords("Event")
-        // .page({ offset: 0, limit: 1 })
-        .options({ include: ["venue"] })
-    );
-    data[1] = await query;
-
-    query = sources.venues[0].query((q) =>
-      q
-        .findRecords("Ticket")
-        // .page({ offset: 0, limit: 1 })
-        .options({ include: ["event"] })
-    );
-    data[2] = await query;
-
-    res.json(data);
   });
-
 
 exports.queryUpdatedCatering = functions
   .region("europe-west1")
--- src/api/src/orbit/sources.ts
+++ src/api/src/orbit/sources.ts
@@ -1,7 +1,41 @@
-import JSONAPISource from "@orbit/jsonapi";
-import { caterVendor, transportVendor, venueVendor } from "./schemas";
+// @ts-nocheck
+import JSONAPISource, {
+  JSONAPIURLBuilder,
+  RecordQueryRequest,
+  RecordTransformRequest,
+} from "@orbit/jsonapi";
 
+import { caterVendor, transportVendor, venueVendor } from "./schemas";
+import { FilterSpecifier } from "@orbit/records";
+import { Dict } from "@orbit/utils";
+
+class FilterJSONAPIURLBuilder extends JSONAPIURLBuilder {
+  buildFilterParam(
+    filters: FilterSpecifier[] | Dict<unknown | unknown[]>,
+    request?: RecordQueryRequest | RecordTransformRequest
+  ): Dict<unknown>[] {
+    const params = [];
+    filters.forEach((filterSpecifier, index) => {
+      if (
+        filterSpecifier.kind === "attribute" &&
+        ["gt", "lt", "gte", "lte"].includes(filterSpecifier.op)
+      ) {
+        const attributeFilter = filterSpecifier;
+        const field = this.serializeFieldParam(attributeFilter.attribute, {
+          kind: "attribute",
+        });
+        params.push(
+          `[{"name":"${field}","op":"${filterSpecifier.op}","val":"${filterSpecifier.value}"}]`
+        );
+      } else {
+        params += super.buildFilterParam(filters, request);
+      }
+    });
+    return params;
+  }
+}
 const caterSource = new JSONAPISource({
+  URLBuilderClass: FilterJSONAPIURLBuilder,
   schema: caterVendor,
   name: "remote",
   host: "http://127.0.0.1:5000",
@@ -14,6 +48,7 @@ const caterSource = new JSONAPISource({
 });
 
 const transportSource = new JSONAPISource({
+  URLBuilderClass: FilterJSONAPIURLBuilder,
   schema: transportVendor,
   name: "remote",
   host: "http://127.0.0.1:5000",
@@ -26,6 +61,7 @@ const transportSource = new JSONAPISource({
 });
 
 const venueSource = new JSONAPISource({
+  URLBuilderClass: FilterJSONAPIURLBuilder,
   schema: venueVendor,
   name: "remote",
   host: "http://127.0.0.1:5000",
