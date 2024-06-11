b7d44328bf2e667397b1c8548aa6b5893831e7f2
Hassin Jalap <hassinahmed9@gmail.com>
2024-06-11 03:03:10 +0200

    trying to add ticketsss

--- src/api/src/functions/endpoints.ts
+++ src/api/src/functions/endpoints.ts
@@ -34,10 +34,37 @@ exports.getFood = onCall({ region: "europe-west1" }, async (request) => {
 });
 
 exports.reserve = onCall({ region: "europe-west1" }, async (request) => {
+  console.log(request.data)
+  console.log(request.data.event.ref.split("/"))
+  console.log(request.data.event.ref.split("/")[1]);
+  console.log(sources.venues[0])
   const eventVendor = await getRef("vendors")
     .doc(request.data.event.ref.split("/")[1])
     .get();
+    console.log(eventVendor)
+
+  console.log("price is: ")
+  console.log(request.data.event.max_price)
   
-  // const correctVendor = sources.venues[0];
-  // correctVendor.update((t)=> t.addRecord)
+  const correctVendor = sources.venues[0];
+
+  const newTicket = {
+    type: "Ticket", // Ensure the type matches your schema
+    attributes: {
+      price: request.data.event.max_price,
+      sold_date: new Date().toISOString(), // Set current date for sold_date
+      status: "reserved",
+      user_id: "userId", // Use the extracted user ID
+      event_id: parseInt(request.data.event.id, 10),
+      created_at: new Date().toISOString(), // Set created_at to current date
+      modified_at: new Date().toISOString(), // Set modified_at to current date
+      removed: "null", // Assuming this field is nullable and not yet removed
+    },
+    relationships: {
+      event: { data: { type: "event", id: request.data.event.id } },
+    },
+  };
+  console.log(newTicket);
+
+  correctVendor.update((t)=> t.addRecord(newTicket))
 });
